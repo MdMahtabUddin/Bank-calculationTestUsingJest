738d540bba0b247fcdaa8966b4d7a2895e795210
Md Mahtab Uddin <mahtab.sentu@outlook.com>
2022-09-02 23:34:05 +0600

    added email

---
+++ bank.js
@@ -0,0 +1,6 @@
+document.getElementById('submit-btn').addEventListener( 'click', function(){
+    // console.log('hello bank');
+    const emailfield=document.getElementById('user-email');
+    const userEmail = emailfield.value;
+    console.log(userEmail);
+})
