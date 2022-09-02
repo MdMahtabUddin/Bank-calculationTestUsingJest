59e3b11d9bc7062737cbb747db38d8cfaaeb5940
Md Mahtab Uddin <mahtab.sentu@outlook.com>
2022-09-03 02:54:37 +0600

    added

--- bank.js
+++ bank.js
@@ -1,6 +1,19 @@
+// PH -26.3 Add event listener navigate to another page
+
 document.getElementById('submit-btn').addEventListener( 'click', function(){
-    // console.log('hello bank');
+    // addevent to emaail
     const emailfield=document.getElementById('user-email');
     const userEmail = emailfield.value;
-    console.log(userEmail);
+    // add event to passard 
+    const passwardField =document.getElementById('user-passward');
+    const userPassward =passwardField.value;
+    // set email & passward condition 
+    if(userEmail =='mahtab@gmail.com' && userPassward == 'secret'){
+        console.log('valid user');
+
+        // navigate another page 
+        window.location.href ='banking.html';
+    }
+
+
 })
