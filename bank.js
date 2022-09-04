26f4c51f20cc2e58adeaec73e5325e08564558dd
MdMahtabUddin <mahtab.sentu@outlook.com>
2022-09-04 14:09:27 +0600

    edit

--- bank.js
+++ bank.js
@@ -1,13 +1,25 @@
 // PH 26-6 Get User deposit and set deposit total value
 // handle deposite button handle 
 document.getElementById('deposite-btn').addEventListener('click',function(){
-    // console.log('hello')
+    
 
     const deposite =depositeInput.value;
-    // console.log(deposite)
+    
     const depositeAmount = document.getElementById('deposite-total');
     depositeAmount.innerText =deposite;
 
     // clear deposite input field 
     depositeInput.value ='';
+
+    // 26-7 Update deposit and balance and handle string add
+
+const previousDepositeAmount =depositeAmount.innerText;
+const newDepositeAmount = parseFloat(previousDepositeAmount) +deposite;
+depositeAmount.innerText =newDepositeAmount;
 })
+
+
+// 26-8 Money withdraw and reduce balance for withdraw
+// handle event withdraw 
+
+
